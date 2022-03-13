ec5e06f543e36523a48f351fc0b76baeeaec40bb
jbenavidesnati05 <jorgenati05@gmail.com>
2022-03-12 23:03:02 -0500

    estadistica

---
+++ estadistica.js
@@ -0,0 +1,13 @@
+
+
+
+function cal_media_aritmetica(lista){
+
+                let sumalista=0
+                for(let i=0; i<lista.length;i++){
+                sumalista= sumalista+lista[i];
+                }
+                const prom_lista =sumalista /lista.length;
+                return prom_lista;
+
+}
